f9d298ec9a4bce95f8a7a1e3582e6ae8304961d6
unknown <you@example.com>
2021-11-24 20:35:18 +0800

    TodoItem的一半

--- src/component/todo.tsx
+++ src/component/todo.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './todo.scss'
 import TodoInputPart from './todoInputPart'
 import axios from '../config/axios'
+import TodoItem from './todoItem'
 
 interface ITodoState{
     todos:any[];
@@ -16,8 +17,10 @@ class Component extends React.Component<any,ITodoState>{
     }
         
     addTodo=async (params:any)=>{
+        const {todos} =this.state
        try{
          const response= await axios.post('todos',params)
+         this.setState({todos:[response.data.resource,...todos]})
          console.log(response.data)
        }catch(e){
          console.log('fail')
@@ -26,30 +29,47 @@ class Component extends React.Component<any,ITodoState>{
     getTodo = async () => { 
         try{
             const response= await axios.get('todos')
-            this.setState({todos:response.data.resource})
+            this.setState({todos:response.data.resources})
         }catch(e){
            throw new Error(e)
         }
     }
+    updateTodo= async (id:number,params:any)=>{
+        const {todos} =this.state
+        try{
+            const  response= await axios.put(`todos/${id}`,params)
+            const newTodos=todos.map(t=>{
+                if(id===t.id){
+                    return response.data.resource
+                }else{
+                    return t
+                }
+            })
+            this.setState({todos:newTodos})
+          }catch(e){
+            throw new Error(e)
+          }
+    }
     componentDidMount(){
         this.getTodo()
     }
-
+   
 
     render(){
         return(
             <div id="Todos">
             <div className='todo1' id='todo1'>番茄时钟区</div>
             <TodoInputPart addTodo={(params)=>this.addTodo(params)} />
-            <main>
-                {
-                    this.state.todos.map(t=>{
-                        return <div key={t.id}>{t.description}</div>
-                    }) 
-                }
-            </main>
             <div className='todo3' id='todo3'>3</div>
             <div className='todo4' id='todo4'>4</div>
+            <div>{ this.state.todos.map(t=>{  
+                    return <TodoItem key={t.id} {...t} 
+                    update={this.updateTodo}
+                    />  
+                  }
+                 ) 
+                }  
+            </div>
             </div>
         )
     }
--- src/component/todoInputPart.tsx
+++ src/component/todoInputPart.tsx
@@ -20,11 +20,12 @@ class Component extends React.Component<ITodoInputProps,ITodoInputState>{
     onKeyUp=(e)=>{
         if(e.keyCode===13 && this.state.description!==''){
             this.addTodo()
-            this.setState({description:''})
+           
         }
     }
     addTodo=()=>{
         this.props.addTodo({description:this.state.description})
+        this.setState({description:''})
     }
 
     public  render(){
